a3b347c6ed0f0461b1b2de170ff9cdda856adc95
Vivek Kumar Singh <vks.guys@gmail.com>
2020-01-24 01:22:11 +0530

    common-action-change-in-dashboard

--- src/pages/index/index.component.jsx
+++ src/pages/index/index.component.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './index.styles.scss'
 import { Button, Success } from '../../components/common/button';
+import { getDashboardList, getDashboardById } from '../../redux/dashboard/dashboard.actions';
 
 class Index extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    };
+  };
+
+  componentDidMount(){
+    this.props.getDashboardList();
+  }
+  
   render() {
     return (
       <>
@@ -10,9 +24,23 @@ class Index extends Component {
       <div className="div">Home</div>
       <Button>Index</Button>
       <Success>Index Success</Success>
+      <select onChange={this.onChangeHandler}>
+          <option>--- Select One---</option>
+          {this.props.dashboardList && this.props.dashboardList.map(item => {
+            return <option key={item.id} value={item.id}>{item.title}</option>
+          })}
+        </select>
       </>
     );
   }
 }
 
-export default Index;
+const mapStateToProps = (state) => {
+  const { dashboardList, dashboardById } = { ...state.dashboard }
+  const { isLoader } = { ...state.common }
+  return {
+    dashboardList, dashboardById, isLoader
+  }
+}
+
+export default connect(mapStateToProps, { getDashboardList, getDashboardById })(Index);
--- src/redux/dashboard/dashboard.actions.js
+++ src/redux/dashboard/dashboard.actions.js
@@ -1,4 +1,5 @@
 import * as dashboardTypes from './dashboard.types';
+import * as commonTypes from '../common/common.types';
 import { Get } from '../../services/services.data';
 import { config } from '../../config';
 
@@ -18,9 +19,11 @@ const dispatchAction = (dispatch, type, data) => {
 
 export const getDashboardList = () => async dispatch => {
     try {
+        dispatchAction(dispatch, commonTypes.LOADING_SHOW, true);
         const data = await Get(`${config.BASE_URL}posts`);
-
+        
         dispatchAction(dispatch, dashboardTypes.DASHBOARD_LIST, data);
+        dispatchAction(dispatch, commonTypes.LOADING_SHOW, false);
     } catch (error) {
 
     }
@@ -28,9 +31,11 @@ export const getDashboardList = () => async dispatch => {
 
 export const getDashboardById = (id) => async dispatch => {
     try {
+        dispatchAction(dispatch, commonTypes.LOADING_SHOW, true);
         const data = await Get(`${config.BASE_URL}posts/${id}`);
 
         dispatchAction(dispatch, dashboardTypes.DASHBOARD_BY_ID, data);
+        dispatchAction(dispatch, commonTypes.LOADING_SHOW, false);
 
     } catch (error) {
 
